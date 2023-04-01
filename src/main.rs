1e7cb5fbe0e10c473ecf00f52080e27650eba63b
jerome <jerome.arn@hotmail.fr>
2023-04-01 10:53:00 +0200

    office MRU and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use datetime::*;
 use std::io;
 use std::mem::transmute;
+use std::time::Duration;
 use winapi::um::minwinbase::SYSTEMTIME;
 use windows::{Win32::System::Com::*, Win32::UI::Shell::Common::*, Win32::UI::Shell::*};
 use winreg::enums::*;
+use winreg::EnumValues;
 use winreg::RegKey;
 use winreg::RegValue;
 
@@ -76,28 +78,13 @@ fn main() -> io::Result<()> {
                 println!("Live ID: {}\nFile MRU:", live_id);
                 // For File MRU
                 let file_mru = office_product_key.open_subkey(format!("{}\\File MRU", live_id))?;
-                for (name, value) in file_mru.enum_values().map(|x| x.unwrap()) {
-                    if name.contains("FOLDERID") {
-                        continue;
-                    }
-                    println!("{name} {value}");
-                    let start_timestamp: usize = value.to_string().find("T").unwrap();
-                    let timestamp =
-                        value.to_string()[start_timestamp + 1..start_timestamp + 17].to_string();
-                    let timestamp_i64 = i64::from_str_radix(&timestamp, 16).unwrap();
-                    println!("{}", timestamp_i64);
-                }
+                iter_list_with_office_mru(file_mru);
 
                 println!("\nPlace MRU:");
                 // For Place MRU
                 let place_mru =
                     office_product_key.open_subkey(format!("{}\\Place MRU", live_id))?;
-                for (name, value) in file_mru.enum_values().map(|x| x.unwrap()) {
-                    if name.contains("FOLDERID") {
-                        continue;
-                    }
-                    println!("{name} {value}");
-                }
+                iter_list_with_office_mru(place_mru);
                 println!("");
             }
         }
@@ -105,19 +92,29 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+pub fn iter_list_with_office_mru(regkey: RegKey) {
+    for (name, value) in regkey.enum_values().map(|x| x.unwrap()) {
+        if name.contains("FOLDERID") {
+            continue;
+        }
+
+        let mut start_timestamp: usize = value.to_string().find("T").unwrap();
+        let timestamp = value.to_string()[start_timestamp + 1..start_timestamp + 17].to_string();
+        let timestamp_u64 = u64::from_str_radix(&timestamp, 16).unwrap();
+        start_timestamp = value.to_string().find("*").unwrap();
+        print!("MRU Time: {}\t MRU location: ", rawvalue_to_timestamp(timestamp_u64));
+        println!(
+            "{}",
+            value.to_string()[start_timestamp + 1..value.to_string().len() - 1].to_string()
+        );
+    }
+}
+
 pub fn iter_list_with_mru_sf(regkey: RegKey) {
-    let raw_last_write_time = regkey.query_info().unwrap().get_last_write_time_system();
-    print!("Last write : ");
-    print_systemtime(raw_last_write_time);
+    print_systemtime(regkey.query_info().unwrap().get_last_write_time_system());
 
     // find MRUListEx to get order of usage
-    let mut mru_position = Vec::new();
-    for (name, value) in regkey.enum_values().map(|x| x.unwrap()) {
-        if name == "MRUListEx" {
-            mru_position = u8_array_to_u32_vec(value);
-            break;
-        }
-    }
+    let mru_position = get_mru_order(regkey.enum_values());
 
     unsafe {
         let mut _com = CoInitialize(None).unwrap();
@@ -159,29 +156,24 @@ pub fn iter_list_with_mru_sf(regkey: RegKey) {
 }
 
 pub fn iter_list_with_mru_rd(regkey: RegKey) {
-    let raw_last_write_time = regkey.query_info().unwrap().get_last_write_time_system();
-    print!("Last write : ");
-    print_systemtime(raw_last_write_time);
+    print_systemtime(regkey.query_info().unwrap().get_last_write_time_system());
 
     // find MRUListEx to get order of usage
-    let mut mru_position = Vec::new();
-    for (name, value) in regkey.enum_values().map(|x| x.unwrap()) {
-        if name == "MRUListEx" {
-            mru_position = u8_array_to_u32_vec(value);
-            break;
-        }
-    }
+    let mru_position = get_mru_order(regkey.enum_values());
+
     println!("MRU position\t| Number\t| Value\n----------------------------------------------");
     for (name, value) in regkey.enum_values().map(|x| x.unwrap()) {
         if name == "MRUListEx" {
             continue;
         }
+
         // convert the value in array of u32
         let mut byte_array: Vec<u16> = Vec::new();
         for x in (0..value.bytes.len()).step_by(2) {
             let a: [u8; 2] = value.bytes[x..x + 2].try_into().unwrap();
             byte_array.push(unsafe { transmute::<[u8; 2], u16>(a) }.to_le())
         }
+
         // the value contain filename_utf16, filname.lnk_utf8, filename.lnk_utf16
         // We take the first and the last value and we clean it
         let split_array = byte_array.split_at(byte_array.iter().position(|x| *x == 0u16).unwrap());
@@ -210,18 +202,10 @@ pub fn iter_list_with_mru_rd(regkey: RegKey) {
 }
 
 pub fn iter_list_with_mru(regkey: RegKey) {
-    let raw_last_write_time = regkey.query_info().unwrap().get_last_write_time_system();
-    print!("Last write : ");
-    print_systemtime(raw_last_write_time);
+    print_systemtime(regkey.query_info().unwrap().get_last_write_time_system());
 
     // find MRUListEx to get order of usage
-    let mut mru_position = Vec::new();
-    for (name, value) in regkey.enum_values().map(|x| x.unwrap()) {
-        if name == "MRUListEx" {
-            mru_position = u8_array_to_u32_vec(value);
-            break;
-        }
-    }
+    let mru_position = get_mru_order(regkey.enum_values());
 
     for (name, value) in regkey.enum_values().map(|x| x.unwrap()) {
         if name == "MRUListEx" {
@@ -258,8 +242,9 @@ pub fn u8_array_to_u32_vec(value: RegValue) -> Vec<u32> {
 }
 
 pub fn print_systemtime(system_time_val: SYSTEMTIME) {
+    print!("Last write : ");
     print!(
-        "{:2}.{:2}.{:2} ",
+        "{}.{}.{} ",
         system_time_val.wDay, system_time_val.wMonth, system_time_val.wYear
     );
     match system_time_val.wDayOfWeek {
@@ -273,18 +258,41 @@ pub fn print_systemtime(system_time_val: SYSTEMTIME) {
         _ => println!(""),
     }
     println!(
-        "{:2}:{:2}:{:2} ",
+        "{}:{}:{} ",
         system_time_val.wHour, system_time_val.wMinute, system_time_val.wSecond
     );
 }
 
-pub fn rawvalue_to_timestamp(tmp: String) -> LocalDateTime {
-    let hex_to_i64: i64 = 0i64;
-    let nanos_to_secs: i64 = hex_to_i64;
+pub fn rawvalue_to_timestamp(tmp: u64) -> String {
+    let nanos_to_secs: i64 = Duration::from_nanos(tmp * 100)
+        .as_secs()
+        .try_into()
+        .unwrap();
     let windows_base_date = LocalDate::ymd(1601, Month::January, 1).unwrap();
-    let hour: i8 = 0;
-    let minute: i8 = 0;
-    let windows_base_time = LocalTime::hm(hour, minute).unwrap();
+    let windows_base_time = LocalTime::hm(0i8, 0i8).unwrap();
     let windows_base_timestamp = LocalDateTime::new(windows_base_date, windows_base_time);
-    windows_base_timestamp.add_seconds(nanos_to_secs)
+    let mut string_vec: Vec<String> = Vec::new();
+    windows_base_timestamp
+        .add_seconds(nanos_to_secs)
+        .iso()
+        .to_string()
+        .split("T")
+        .for_each(|x| string_vec.push(x.to_string()));
+    format!(
+        "{} {}",
+        string_vec.get(0).unwrap(),
+        string_vec.get(1).unwrap()
+    )
+}
+
+pub fn get_mru_order(mru_values: EnumValues<'_>) -> Vec<u32> {
+    // find MRUListEx to get order of usage
+    let mut mru_position = Vec::new();
+    for (name, value) in mru_values.map(|x| x.unwrap()) {
+        if name == "MRUListEx" {
+            mru_position = u8_array_to_u32_vec(value);
+            break;
+        }
+    }
+    mru_position
 }
